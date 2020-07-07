09f29b253ed9d29032b24ddca8921453b46887b3
Moinuddin <Muhammad Moinuddin>
2020-07-07 21:06:32 +0500

    first commit

---
+++ beginner.py
@@ -0,0 +1,5 @@
+a = int(input("Please enter first number: "))
+b = int(input("Please enter Second number: "))
+if a > b : print('{0} is the largest'.format(a))
+else : print('{0} is the largest'.format(b))
+    
