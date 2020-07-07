af611f606063bf9fcdf94a364bedf347686e2802
Muhammad Moinuddin <Muhammad Moinuddin>
2020-07-07 21:36:45 +0500

    Update beginner.py
    Input a positive or negative

--- beginner.py
+++ beginner.py
@@ -2,4 +2,6 @@ a = int(input("Please enter first number: "))
 b = int(input("Please enter Second number: "))
 if a > b : print('{0} is the largest'.format(a))
 else : print('{0} is the largest'.format(b))
-    
+#First number Positive or negative
+if a > 0 :  print('{0} is Positive'.format(a)) 
+else :  print('{0} is Negative'.format(a))  
