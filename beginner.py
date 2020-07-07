5e04785c80618a915aeb0bb8fb36a3f87888c0a6
Muhammad Moinuddin <Muhammad Moinuddin>
2020-07-07 22:27:25 +0500

    Update beginner.py
    added even or odd feature

--- beginner.py
+++ beginner.py
@@ -5,3 +5,9 @@ else : print('{0} is the largest'.format(b))
 #First number Positive or negative
 if a > 0 :  print('{0} is Positive'.format(a)) 
 else :  print('{0} is Negative'.format(a))  
+#First number greater or smaller than 100
+if a > 100 :  print('{0} is greater than 100'.format(a))
+else : print ('{0} is smaller than 100'.format(a))
+#First number even or odd
+if a % 2 == 0 : print ('{0} is even'.format(a))
+else : print ('{0} is odd'.format(a))
