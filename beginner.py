1aaef41a295d583e67bf947d9f30b8890c052ce6
Muhammad Moinuddin <Muhammad Moinuddin>
2020-07-08 07:19:45 +0500

    Update beginner.py
    added division and multiple part

--- beginner.py
+++ beginner.py
@@ -2,12 +2,18 @@ a = int(input("Please enter first number: "))
 b = int(input("Please enter Second number: "))
 if a > b : print('{0} is the largest'.format(a))
 else : print('{0} is the largest'.format(b))
-#First number Positive or negative
+#First input Positive or negative
 if a > 0 :  print('{0} is Positive'.format(a)) 
 else :  print('{0} is Negative'.format(a))  
-#First number greater or smaller than 100
+#First input greater or smaller than 100
 if a > 100 :  print('{0} is greater than 100'.format(a))
 else : print ('{0} is smaller than 100'.format(a))
-#First number even or odd
+#First input even or odd
 if a % 2 == 0 : print ('{0} is even'.format(a))
 else : print ('{0} is odd'.format(a))
+#First input divisible by 5 or not
+if a % 5 == 0 : print ('{0} is divisible by 5'.format(a))
+else : print ('{0} is not divisible by 5'.format(a))
+#First input multiple of 7 or not
+if a % 7 == 0 : print ('{0} is multiple of 7'.format(a))
+else : print ('{0} is not multiple of 7'.format(a))
